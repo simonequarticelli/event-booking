107db26dcbc99cbf6ea616d31fad95eec8983efa
Quarticelli <quarticelli.simone@gmail.com>
2023-12-28 15:44:51 +0100

    Create tables, models, and controllers for Event, Place, Ticket, and Transaction

---
+++ database/migrations/2023_12_28_122302_create_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Event;
+use App\Models\TicketState;
+use App\Models\Transaction;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(Event::class)->constrained();
+            $table->foreignIdFor(TicketState::class)->constrained();
+            $table->foreignUuid('transaction_id')->constrained();
+            $table->unsignedFloat('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
