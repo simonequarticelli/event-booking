7bdf1813333cca0a2b8d487063ceec90346e1025
Quarticelli <quarticelli.simone@gmail.com>
2023-12-30 21:42:37 +0100

    Refactor UserService to use authenticated user

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,15 +3,13 @@
 namespace App\Services;
 
 use App\Models\TransactionState;
-use App\Models\User;
 
 class UserService
 {
     public static function pendingTransaction()
     {
-        // auth()->user()
-        return User::find(1)->transactions()->with('state', function ($query) {
-            $query->where('slug', [TransactionState::PENDING]);
+        return auth()->user()->transactions()->whereHas('state', function ($query) {
+            $query->where('slug', TransactionState::PENDING);
         });
     }
 
