ac622a35a84b667b72a780510a9d2699c208c638
Quarticelli <quarticelli.simone@gmail.com>
2023-12-30 01:50:11 +0100

    Add tickets availability attribute to Event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -12,6 +12,22 @@ class Event extends Model
 {
     use HasFactory;
 
+    protected $appends = ['tickets_available'];
+
+    public function scopeGreaterThanToday($query)
+    {
+        return $query->where('start_date', '>', now());
+    }
+
+    public function getTicketsAvailableAttribute()
+    {
+        $reservedTickets = $this->tickets()->with('state', function ($query) {
+            $query->whereIn('slug', ['pending', 'approved']);
+        })->count();
+
+        return ($this->seats - $reservedTickets);
+    }
+
     public function price(): hasOne
     {
         return $this->hasOne(EventPrice::class);
