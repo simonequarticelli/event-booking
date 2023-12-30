1a9edd38e400eb5fed885c1d8454d30566308862
Quarticelli <quarticelli.simone@gmail.com>
2023-12-30 01:51:28 +0100

    Add Event and Reservation Controllers to routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\ReservationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
+
+Route::post('/reservation', ReservationController::class);
+
+Route::resources([
+    'events' => EventController::class,
+]);
